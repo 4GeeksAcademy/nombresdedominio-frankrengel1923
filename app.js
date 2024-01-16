58dfb0301b612e5bd1496f237c8ca7a3d876e0ee
frankluis1923 <148373205+frankluis1923@users.noreply.github.com>
2024-01-16 19:34:47 +0000

    nombresdedominio

---
+++ app.js
@@ -0,0 +1,15 @@
+let pronoun = ['the','cat','joe','our'];
+let adj = ['great','jogger', 'big','racoon'];
+let tercera= ['carlos','jose','javier'];
+
+
+for (let index = 0; index < pronoun.length; index++) {
+   
+    for (let j = 0; j < adj.length; j++) {
+        
+        
+    for (let z = 0; z < tercera.length; z++) {
+    console.log(pronoun[index]+adj[j]+tercera[z]+".com");}
+        
+    }
+}
